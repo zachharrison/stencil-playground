36bd50eaa191445f025a3a9799dc5e022e9efbe5
Zach Harrison <zharrison@shaw.ca>
2021-06-09 15:09:47 -0700

    Adjusted counter styling so component is more reusable

--- playground/src/components/playground-counter/playground-counter-view.tsx
+++ playground/src/components/playground-counter/playground-counter-view.tsx
@@ -12,10 +12,10 @@ export interface PlaygroundCounterViewProps {
 export function PlaygroundCounterView(props: PlaygroundCounterViewProps) {
   const { background, count, increment, decrement, incrementBy, decrementBy } = props;
   return (
-    <div class={`counter-container background-${background}`}>
+    <div class={`counter-container background-${background} mb-3`}>
       <h1 class="count">{count}</h1>
       <div class="flex-center">
-        <playground-button onClick={increment} text={`+${incrementBy}`} type="primary"></playground-button>
+        <playground-button onClick={increment} text={`+${incrementBy}`} type={`${background === 'primary' ? 'secondary' : 'primary'}`}></playground-button>
         <playground-button onClick={decrement} text={`-${decrementBy}`} type="dark"></playground-button>
       </div>
     </div>
